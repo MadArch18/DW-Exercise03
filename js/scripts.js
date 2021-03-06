d985115d2e8ea50efe5f345be0c8c76471b15083
Madeline Archibald <madeline.archibald@uleth.ca>
2021-03-06 15:40:47 -0700

    polished order, mobile-friendly. last minute touch-ups

--- js/scripts.js
+++ js/scripts.js
@@ -23,69 +23,74 @@ function Picture(url, title, tags, color) {
 var pics = [
     new Picture('"images/maeve1.jpg"',
         'Maeve pre-prom arrival',
-        ["Maeve", "Solo", "Reset"],
+        ["Reset", "Maeve", "Solo"],
         '#D3B9B8'),
     new Picture('"images/maeve2.jpg"',
         'Maeve closeup pre-prom arrival',
-        ["Maeve", "Solo", "Reset"],
+        ["Reset", "Maeve", "Solo"],
         '#D3B9B8'),
     new Picture('"images/neva1.jpg"',
         'Neva closeup',
-        ["Neva", "Solo", "Reset"],
+        ["Reset", "Neva", "Solo"],
         '#D1CEC7'),
     new Picture('"images/neva2.jpg"',
         'Neva closeup',
-        ["Neva", "Solo", "Reset"],
+        ["Reset", "Neva", "Solo"],
         '#D1CEC7'),
     new Picture('"images/claire1-group.jpg"',
         'Claire with prom date Ben',
-        ["Claire", "Group-shots", "Reset"],
+        ["Reset", "Claire", "Group-shots"],
         '#EEDBD7'),
     new Picture('"images/group3-claire.jpg"',
         'Claire and Emma',
-        ["Claire", "Group-shots", "Reset"],
+        ["Reset", "Claire", "Group-shots"],
         '#EEDBD7'),
     new Picture('"images/maeve-neva-claire.jpg"',
         'Claire, Maeve, and Neva',
-        ["Claire", "Maeve", "Neva", "Group-shots", "Reset"],
+        ["Reset", "Claire", "Maeve", "Neva", "Group-shots"],
         '#9D9C98'),
     new Picture('"images/group1.jpg"',
         'Admiring corsages',
-        ["Group-shots", "Reset"],
+        ["Reset", "Group-shots"],
         '#9D9C98'),
     new Picture('"images/group2.jpg"',
         'Girls group shot',
-        ["Group-shots", "Claire", "Maeve", "Neva", "Reset"],
+        ["Reset", "Group-shots", "Claire", "Maeve", "Neva"],
         '#9D9C98'),
     new Picture('"images/group4.jpg"',
         'Students group shot',
-        ["Group-shots", "Claire", "Maeve", "Neva", "Reset"],
+        ["Reset", "Group-shots", "Claire", "Maeve", "Neva"],
         '#9D9C98')
                 ]
+$(".title").text("Photo Gallery - Prom 2019");
 
 //global taglist
 var tagList = []
 
 pics.forEach(function (picture) {
     picture.display();
+    tagList.sort();
 
     picture.tags.forEach(function (tag) {
-        //check to see if tag has been added to taglist
         if (!tagList.includes(tag)) {
-            //if it isn't already added, add it
             tagList.push(tag);
-            //also make a button for it.
             $(".buttons").prepend("<button class='filter'>" + tag + "</button>")
-        } //section and option drop down list??
+        }
     })
+
 })
+
+
 
 console.log(tagList);
 
 $(".filter").on("click", function () {
-    var tag = $(this).text(); //or attr.id
+    var tag = $(this).text();
     console.log(tag);
     $("div > div").not("." + tag).hide();
     $("." + tag).fadeIn();
+
+    $(".filter").removeClass("active");
+    $(this).addClass("active");
 
 })
